3ab7c8ca90fdb5309f736d74bab04d20c0ce5190
m18317252500 <m18317252500_1@163.com>
2019-12-24 17:00:55 +0800

    update nger graphql

--- packages/pg/lib/pg_graphql.ts
+++ packages/pg/lib/pg_graphql.ts
@@ -1,27 +1,21 @@
 import { SchemaBuilder } from "@nger/graphql";
 export { sql } from 'graphile-build-pg';
-import { graphql, ExecutionResult, Source } from "graphql";
-import pg from "pg";
-import { Driver } from "@nger/orm";
+import { graphql } from "graphql";
 import { Injectable, Injector } from "@nger/core";
 
 @Injectable()
 export class PgGraphql {
-    private driver: Driver<pg.Pool>;
     constructor(
-        private injector: Injector
+        private schemaBuilder: SchemaBuilder
     ) { }
-    async query<T>(query: string | Source, variables: any = {}): Promise<ExecutionResult<T>> {
-        const builder = this.injector.get(SchemaBuilder)
-        return await graphql(
-            await builder.buildSchema(),
-            query,
-            await builder.buildRoot(),
-            {
-                pgClient: this.driver.master,
-                injector: this.injector
-            },
-            variables
-        );
+    async run(query: string, variables: any = {}) {
+        const schema = await this.schemaBuilder.buildSchema();
+        return graphql({
+            schema,
+            source: query,
+            rootValue: await this.schemaBuilder.buildRoot(),
+            contextValue: await this.schemaBuilder.buildContext(),
+            variableValues: variables
+        });
     }
 }
